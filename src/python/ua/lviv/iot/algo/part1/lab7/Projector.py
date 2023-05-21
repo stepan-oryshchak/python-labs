a1175db389b4f611aec0ce2d978bfa4b4334892a
stepan-oryshchak <zavaplayyt@gmail.com>
2023-05-21 20:13:58 +0300

    added main file

--- src/python/ua/lviv/iot/algo/part1/lab7/Projector.py
+++ src/python/ua/lviv/iot/algo/part1/lab7/Projector.py
@@ -1,7 +1,16 @@
 class Projector:
+    """
+    Represent a Projector class:
+
+    Methods:
+        get_instance(): Returns instance of the Projector class.
+        add_input_device(device: str): Adds new type of devices.
+        disconnect_device(): Disconnect device.
+        increase_lamp_hours(lamp_hour: int): Increase the lamp hours.
+    """
     default_projector = None
 
-    def __init__(self, model="", connected_device="", resolution="", lamp_hours=0):
+    def __init(self, model="", connected_device="", resolution="", lamp_hours=0):
         self.model = model
         self.connected_device = connected_device
         self.resolution = resolution
@@ -9,9 +18,13 @@ class Projector:
 
     @staticmethod
     def get_instance():
-        if Projector.default_projector is None:
-            Projector.default_projector = Projector()
-        return Projector.default_projector
+        if not Projector.default_projector:
+            Projector.__default_projector = Projector()
+        return Projector.__default_projector
+
+    @staticmethod
+    def print ():
+        print("Hello")
 
     def add_input_device(self, device):
         self.connected_device = device
@@ -25,27 +38,6 @@ class Projector:
         self.lamp_hours += hours
         return self.lamp_hours
 
-    def __str__(self):
-        return f"Model: {self.model}, " \
-               f"Connected Device: {self.connected_device}, " \
-               f"Resolution: {self.resolution}, " \
-               f"Lamp Hours: {self.lamp_hours}"
-
-
-if __name__ == '__main__':
-    first_projector = Projector()
-    second_projector = Projector("Lenovo", "HDMI", "2420x2420", 12)
-    third_projector = Projector("Acer", "VGA", "1000x1000", 7)
-    fourth_projector = Projector.get_instance()
-
-    projectors = [first_projector, second_projector, third_projector, fourth_projector]
-
-    for projector in projectors:
-        print(projector)
-
-    second_projector.add_input_device("DVI")
-    second_projector.increase_lamp_hours(3)
-    print(second_projector)
-
-    third_projector.disconnect_device()
-    print(third_projector)
+    def __str(self):
+        return f"Model: {self.model}, " f"Connected Device: {self.connected_device}, " \
+               f"Resolution: {self.resolution}, " f"Lamp Hours: {self.lamp_hours}"
